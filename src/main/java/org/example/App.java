b9337714c6fd5f2dcc25da078da7c7d84fc58bf5
A.Mathew <BlueOrchid543@gmail.com>
2021-09-13 13:56:18 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,21 @@
+/**
+ * UCF COP3330 Fall 2021 Assignment 1 Solution
+ * Copyright 2021 Austin Mathew
+ */
+
+package org.example;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("What is the quote? ");
+        String quote = input.nextLine();
+        System.out.print("Who said it? ");
+        String name = input.nextLine();
+        System.out.print(name + " says, " + '\"' + quote + '\"');
+
+
+    }
+}
